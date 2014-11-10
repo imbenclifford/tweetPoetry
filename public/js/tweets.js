7bb5101961cbcc50fef0e3fe259880c4b225cf7c
imbenclifford <imbenclifford@gmail.com>
2014-11-10 23:35:30 +0000

    css-styling-and-more

--- public/js/tweets.js
+++ public/js/tweets.js
@@ -1,22 +1,47 @@
 function getWord(data)
-{   
-    var words=data.split(' ');    
-    console.log(words.length);
-    var randNumMax = words.length-1;
+{     
+    var randNumMax = data.length-1;
 var randInt = (Math.floor(Math.random() * (randNumMax + 1)));
-  return words[randInt];
+  return data[randInt];
+}
+
+var at = [];
+var hash = [];
+var long = [];
+
+function sortWords(arr){
+	arr.forEach(function(word){
+		if (word.match(/^@/)){
+			at.push(word)
+		}
+		else if (word.match(/^#/)){
+			hash.push(word)
+		}
+		else if (word.length > 5 && !word.match(/^http/)){
+			long.push(word)
+		}
+	})
 }
 
 $( document ).ready(function() {
 	$.ajax({
 			url: "http://mattwritespoetry.herokuapp.com/",
 			success: function(data) {
+				console.log("ajax is connecting")
 				var Jdata = JSON.parse(data);
+				sortWords(Jdata);						
 				var str = '';
-				console.log(Jdata)
-				for(var i=0; i<Jdata.length; i++) {
-					 str = getWord(Jdata[i])
-					 $( "#tweets" + i ).append(str);
+				for(var i=0; i<at.length; i++) {
+					 str = getWord(at);
+					 $( "#at" + i ).append(str);
+				}
+				for(var i=0; i<hash.length; i++) {
+					 str = getWord(hash);
+					 $( "#hash" + i ).append(str);
+				}
+				for(var i=0; i<long.length; i++) {
+					 str = getWord(long);
+					 $( "#long" + i ).append(str);
 				}
 			}
   });
